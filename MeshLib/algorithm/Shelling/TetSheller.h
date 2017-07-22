3eae88a0543f7858b681fc3f57450bbdc4f34a79
AnkaChan <ankachan@126.com>
2017-07-22 17:49:36 +0800

    Added new method: isReverseShelling(); and some interface functions

--- MeshLib/algorithm/Shelling/TetSheller.h
+++ MeshLib/algorithm/Shelling/TetSheller.h
@@ -24,11 +24,15 @@ namespace MeshLib {
 		class CTetShelling : public CTet
 		{
 		public:
+			int numFacesOnSurfaceInShelling();
 			bool inCandidateList = false;
 			bool inShellingOrder = false;
 			bool visible = false;
 		};
-
+		int MeshLib::TMeshLib::CTetShelling::numFacesOnSurfaceInShelling()
+		{
+			return 0;
+		}
 		template< typename TV, typename V, typename HE, typename TE, typename E, typename HF, typename F, typename T>
 		class CTetSheller {
 		public:
